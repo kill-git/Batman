706e431a1d78fb430ee74402627dc6eb630bc58b
kill-git <132648790+kill-git@users.noreply.github.com>
2025-05-12 17:20:31 +0200

    create pipeline preprocessing

---
+++ src/batman/pipelines/data_preprocessing.py
@@ -0,0 +1,8 @@
+import sys
+import os
+import glob
+import pandas as pd
+
+# Ajoute le dossier parent au path pour importer data_preprocessing.py
+sys.path.append(os.path.abspath(os.path.join('..')))
+
